9aeccf9e770dc4634f7f752584f0fa5c0bb49851
JamesSmith1202 <jsmith@stuy.edu>
2017-12-08 15:04:58 -0500

    gitter dun feller

--- intro.js
+++ intro.js
@@ -1,8 +1,24 @@
 var item = 8;
+var originalHeader = document.getElementById("h").innerHTML;
+
+var unhover = function(){
+    document.getElementById("h").innerHTML = originalHeader;
+};
+
+var hover = function(){
+    document.getElementById("h").innerHTML = this.innerHTML;
+};
+
+var remove = function(){
+    this.remove();
+}
 
 var addItem = function(){
     var list = document.getElementById("thelist");
     var newItem = document.createElement("li");
+    newItem.addEventListener("mouseover", hover);
+    newItem.addEventListener("mouseout", unhover);
+    newItem.addEventListener("click", remove);
     newItem.innerHTML = "item "+item;
     item++;
     list.appendChild(newItem);
@@ -11,4 +27,9 @@ var addItem = function(){
 var button = document.getElementById("b");
 button.addEventListener("click", addItem);
 
-var item = document.getElementsByTagName("li");
+var items = document.getElementsByTagName("li");
+for(var i = 0; i < item; i++){
+    items[i].addEventListener("mouseover", hover);
+    items[i].addEventListener("mouseout", unhover);
+    items[i].addEventListener("click", remove);
+};
